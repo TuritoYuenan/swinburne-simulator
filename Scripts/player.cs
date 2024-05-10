3d431f87502bf40f0d5f97b41b786b16a9457863
katiue <vinhkhhangnguyen38@gmail.com>
2024-05-10 10:18:59 +0700

    done character movement

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -3,8 +3,11 @@ using System;
 
 public partial class player : CharacterBody3D
 {    
+	public float JumpVelocity { get; private set; } = 15.0f;
+	public float Gravity { get; private set; } = -9.8f;
+	public bool jumping = false;
 	public float Speed { get; private set; } = 5.0f;
-	public float MouseSensitivity { get; private set; } = 0.5f;
+	public float MouseSensitivity { get; private set; } = 0.001f;
 	private Vector2 _mouseMotion = new Vector2();
 
 	// Called when the node enters the scene tree for the first time.
@@ -17,12 +20,24 @@ public partial class player : CharacterBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-
 		// Handle Jump.
-		//if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		//    velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
+		
+		if (Input.IsActionPressed("jump"))
+		{
+			if(velocity.Y < JumpVelocity)
+			{
+				jumping = true;
+				velocity.Y = Mathf.MoveToward(JumpVelocity, 0, Speed);
+			}
+			else
+			{
+				jumping = false;
+			}
+		}
+		if(!IsOnFloor()&&!jumping)
+		{
+			velocity.Y = Mathf.MoveToward(0, Gravity, Speed);
+		}
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
@@ -39,8 +54,6 @@ public partial class player : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
-
-		velocity.Y = 0;
 		if (Mathf.IsZeroApprox(velocity.Length())) return;
 
 		//var angle = 0;
@@ -49,12 +62,13 @@ public partial class player : CharacterBody3D
 		//_visual.Transform = transform;
 		
 	}
-	//Rotate Character view
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventMouseMotion mouseEvent)
 		{
-			Rotation= new Vector3(Rotation.X, Rotation.Y - mouseEvent.Relative.X * MouseSensitivity, Rotation.Z );
+			Godot.Node3D child= this.GetNode<Node3D>("Head");
+			child.Rotation= new Vector3(child.Rotation.X, child.Rotation.Y, child.Rotation.Z - mouseEvent.Relative.Y * MouseSensitivity);
+			this.Rotation= new Vector3(Rotation.X, Rotation.Y - mouseEvent.Relative.X * MouseSensitivity, Rotation.Z);
 		}
 	}
 }
